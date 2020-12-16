a8c62f28627d0aa698316f2ceffc31cf7de30b6a
stephendltg <s.deletang@laposte.net>
2020-12-16 10:25:30 +0100

    site

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# site
+# EPTO MODEL VUEJS/VUE-CLI
+
+## Vue UI
+
+```
+npm install -g @vue/cli
+```
+
+### Launch Vue ui
+
+```
+vue ui
+````
+
+---
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+---
+
+# ICON GENERATOR ( if necessary )
+
+```
+npm install --global vue-pwa-asset-generator
+logo.png: 512x512_png
+vue-asset-generate -a logo.png -o img
+
+or
+
+npx vue-pwa-asset-generator -a {512x512_png_source | svg_source} -o {output_folder}
+```
+
+---
+
+# CAPACITOR
+
+```Android
+npx cap add android
+```
+
+``Ìos
+npx cap add android
+```
---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <div class="navbar-fixeds">
+    <nav class="nav-extended  black darken-1 ptm">
+      <div class="nav-wrapper mlm">
+        <a href="#" class="brand-logo">
+          <i class="material-icons large">blur_on</i>ADMIN
+        </a>
+        <a href="#" data-target="slide-out" class="sidenav-trigger">
+          <i class="material-icons">menu</i>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="/" target="_blank">EPYO VISIO</a></li>
+        </ul>
+      </div>
+      <div class="nav-content">
+        <ul class="tabs tabs-transparent">
+          <li class="tab"><a href="#section1">CONFIGURATION</a></li>
+          <li class="tab"><a href="#section2">PARAMS</a></li>
+          <li class="tab"><a href="#section3">INFORMATIONS</a></li>
+          <li class="tab"><a href="#section4">LOGS</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <ul class="sidenav" id="slide-out">
+      <li><a href="/">EPYO</a></li>
+    </ul>
+  </div>
+
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+
+@import "./assets/style.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
